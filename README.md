81669e86e378ececb2b81b8b0427b6b1a55631c4
Calvein <calvein@gmail.com>
2019-10-16 22:43:03 +0200

    :sparkles: First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# pdh-slackbot
-Slackbot using Météo France "pluie dans l'heure" service
+# Pluie dans l'heure Slack App
+
+Slack App using [Météo France "pluie dans l'heure" service](http://www.meteofrance.com/previsions-meteo-france/previsions-pluie)
+
+Built following this article:
+https://zeit.co/blog/serverless-slack-apps-with-now
---
+++ api/index.js
@@ -0,0 +1,24 @@
+const { text } = require('micro')
+const { parse } = require('querystring')
+const getSlackMessage = require('getSlackMessage')
+
+module.exports = async (req, res) => {
+  const body = parse(await text(req)).text
+
+  const result = await getSlackMessage(body)
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(
+    JSON.stringify({
+      response_type: 'in_channel',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: result,
+          },
+        },
+      ],
+    })
+  )
+}
